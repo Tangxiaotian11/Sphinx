e10fa090eaf6ddbd01e0ed9e8d904f921e863ca4
Tangxiaotian11 <tangxiaotian201@mails.ucas.ac.cn>
2021-12-13 13:22:01 +0800

    master

--- source/conf.py
+++ source/conf.py
@@ -195,3 +195,5 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+master_doc = 'index'
